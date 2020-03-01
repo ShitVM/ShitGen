376bf35cc2d3ba38a08425643269c33f082bddb5
kmc7468 <kmc7468@naver.com>
2020-03-01 17:35:24 +0900

    inc, dec 명령어 추가

--- include/sgn/Instruction.hpp
+++ include/sgn/Instruction.hpp
@@ -18,6 +18,8 @@ namespace sgn {
 		Mod,
 		IMod,
 		Neg,
+		Inc,
+		Dec,
 
 		And,
 		Or,
@@ -51,7 +53,7 @@ namespace sgn {
 	static constexpr const char* Mnemonics[] = {
 		"nop",
 		"push", "pop", "load", "store",
-		"add", "sub", "mul", "imul", "div", "idiv", "mod", "imod", "neg",
+		"add", "sub", "mul", "imul", "div", "idiv", "mod", "imod", "neg", "inc", "dec",
 		"and", "or", "xor", "not", "shl", "shr", "sal", "sar",
 		"cmp", "icmp", "jmp", "je", "jne", "ja", "jae", "jb", "jbe", "call", "ret",
 		"i2l", "i2d", "l2i", "l2d", "d2i", "d2l",
