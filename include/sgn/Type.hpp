e65639dea72f542ea883c54f12e217e7faf239ae
kmc7468 <kmc7468@naver.com>
2020-03-01 17:31:47 +0900

    "Type 클래스 구조 변경" 커밋 되돌림
    커밋 d62960dfc03c316726a2e52a67b3b84d31c0f276을 되돌렸습니다.

--- include/sgn/Type.hpp
+++ include/sgn/Type.hpp
@@ -5,50 +5,27 @@
 #include <string_view>
 
 namespace sgn {
-	class TypeData final {
+	class Type final {
 	private:
 		std::string m_Name;
 		std::uint64_t m_Size = 0;
 
 	public:
-		TypeData(std::string name, std::uint64_t size) noexcept;
-		TypeData(const TypeData&) = delete;
-		virtual ~TypeData() = default;
+		Type(std::string name, std::uint64_t size) noexcept;
+		Type(const Type&) = delete;
+		virtual ~Type() = default;
 
 	public:
-		TypeData& operator=(const TypeData&) = delete;
-		bool operator==(const TypeData&) = delete;
-		bool operator!=(const TypeData&) = delete;
+		Type& operator=(const Type&) = delete;
+		bool operator==(const Type&) = delete;
+		bool operator!=(const Type&) = delete;
 
 	public:
 		std::string_view GetName() const noexcept;
 		std::uint64_t GetSize() const noexcept;
 	};
-}
 
-namespace sgn {
-	class Type final {
-	private:
-		const TypeData* m_Data = nullptr;
-
-	public:
-		Type() noexcept = default;
-		Type(std::nullptr_t) noexcept;
-		Type(const TypeData* data) noexcept;
-		Type(const Type& type) noexcept;
-		~Type() = default;
-
-	public:
-		Type& operator=(std::nullptr_t) noexcept;
-		Type& operator=(const TypeData* data) noexcept;
-		Type& operator=(const Type& type) noexcept;
-		bool operator==(const Type& type) const noexcept;
-		bool operator!=(const Type& type) const noexcept;
-		const TypeData& operator*() const noexcept;
-		const TypeData* operator->() const noexcept;
-	};
-
-	extern const Type IntType;
-	extern const Type LongType;
-	extern const Type DoubleType;
+	extern const Type* IntType;
+	extern const Type* LongType;
+	extern const Type* DoubleType;
 }
