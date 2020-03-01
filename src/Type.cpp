d62960dfc03c316726a2e52a67b3b84d31c0f276
kmc7468 <kmc7468@naver.com>
2020-03-01 17:31:47 +0900

    Type 클래스 구조 변경
    기존 Type 클래스의 이름을 TypeData로 변경하고, 새로운 Type 클래스를 만들어 해당 클래스의 객체가 TypeData 클래스에 대한 포인터를 저장하도록 구조를 변경하였습니다.

--- src/Type.cpp
+++ src/Type.cpp
@@ -1,23 +1,56 @@
 #include <sgn/Type.hpp>
 
 namespace sgn {
-	Type::Type(std::string name, std::uint64_t size) noexcept
+	TypeData::TypeData(std::string name, std::uint64_t size) noexcept
 		: m_Name(std::move(name)), m_Size(size) {}
 
-	std::string_view Type::GetName() const noexcept {
+	std::string_view TypeData::GetName() const noexcept {
 		return m_Name;
 	}
-	std::uint64_t Type::GetSize() const noexcept {
+	std::uint64_t TypeData::GetSize() const noexcept {
 		return m_Size;
+	}
+}
+
+namespace sgn {
+	Type::Type(std::nullptr_t) noexcept {}
+	Type::Type(const TypeData* data) noexcept
+		: m_Data(data) {}
+	Type::Type(const Type& type) noexcept
+		: m_Data(type.m_Data) {}
+
+	Type& Type::operator=(std::nullptr_t) noexcept {
+		m_Data = nullptr;
+		return *this;
+	}
+	Type& Type::operator=(const TypeData* data) noexcept {
+		m_Data = data;
+		return *this;
+	}
+	Type& Type::operator=(const Type& type) noexcept {
+		m_Data = type.m_Data;
+		return *this;
+	}
+	bool Type::operator==(const Type& type) const noexcept {
+		return m_Data == type.m_Data;
+	}
+	bool Type::operator!=(const Type& type) const noexcept {
+		return m_Data != type.m_Data;
+	}
+	const TypeData& Type::operator*() const noexcept {
+		return *m_Data;
+	}
+	const TypeData* Type::operator->() const noexcept {
+		return m_Data;
 	}
 
 	namespace {
-		static const Type s_IntType("int", 4);
-		static const Type s_LongType("long", 8);
-		static const Type s_DoubleType("double", 8);
+		static const TypeData s_IntType("int", 4);
+		static const TypeData s_LongType("long", 8);
+		static const TypeData s_DoubleType("double", 8);
 	}
 
-	const Type* IntType = &s_IntType;
-	const Type* LongType = &s_LongType;
-	const Type* DoubleType = &s_DoubleType;
+	const Type IntType = &s_IntType;
+	const Type LongType = &s_LongType;
+	const Type DoubleType = &s_DoubleType;
 }
