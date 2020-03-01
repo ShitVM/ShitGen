a47ede7322383f4029e9bd43e5c772cd5c397e4a
kmc7468 <kmc7468@naver.com>
2020-03-01 21:01:38 +0900

    GetEndian, ReverseEndian 함수 추가

---
+++ include/sgn/Memory.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <sgn/Macro.hpp>
+
+#include <algorithm>
+#include <cstdint>
+
+namespace sgn {
+	enum class Endian {
+		Little,
+		Big,
+	};
+
+#ifdef SVM_LITTLE
+	constexpr Endian GetEndian() noexcept {
+		return Endian::Little;
+	}
+#else
+	Endian GetEndian() noexcept;
+#endif
+
+	template<typename T>
+	T ReverseEndian(const T& value) noexcept {
+		union transformer {
+			std::uint8_t Bytes[sizeof(value)];
+		} temp;
+		temp = *reinterpret_cast<const transformer*>(&value);
+		std::reverse(temp.Bytes, temp.Bytes + sizeof(value));
+		return *reinterpret_cast<T*>(&temp);
+	}
+}
